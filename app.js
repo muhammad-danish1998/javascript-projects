7848c02da89d9e9a766e959b006b84ec23b9479b
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-10-06 17:55:55 +0500

    first chapter

---
+++ app.js
@@ -0,0 +1,72 @@
+// Create the branch on your local machine and switch in this branch :
+
+// $ git checkout -b [name_of_your_new_branch]
+
+// Push the branch on github :
+
+// $ git push origin [name_of_your_new_branch]
+
+// You can see all the branches created by using :
+
+// $ git branch -a
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======================================== 
+
+// ========================= 1 =================== 
+
+// alert(`Welcome To Js Land \n Happy Coding ...`)
+
+// ========================= 2 =================== 
+
+// let name = "John";
+// let age = 25;
+// let tech = `Mobile and Application Developer`;
+
+// alert(name);
+// alert(age);
+// alert(tech);
+
+
+// ========================= 3 =================== 
+
+// let email = `iamdanish1998@gmail.com`
+// alert(`my Email Address is ${email}`)
+
+// ========================= 4 =================== 
+
+
+// document.write("Yeah! i can write Javascript Coding ...")
+
+// ========================= 5 ===================
+
+// let age;
+
+//  age = prompt(`eNTER Your Age`)
+
+//  alert(`i am ${age} years old`)
+
+// ========================= 6 =================== 
+
+
+
+
+
+
+
+
+
+
+
+
