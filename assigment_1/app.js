31f38c4513c1168c199cba95295ff0fd0323620e
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-10-06 21:55:11 +0500

    assigment_1

---
+++ assigment_1/app.js
@@ -0,0 +1,150 @@
+// Create the branch on your local machine and switch in this branch :
+
+// $ git checkout -b [name_of_your_new_branch]
+
+// Push the branch on github :
+
+// $ git push origin [name_of_your_new_branch]
+
+// You can see all the branches created by using :
+
+// $ git branch -a
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======================================== 
+
+// ========================= 1 =================== 
+
+// alert(`Welcome To Js Land \n Happy Coding ...`)
+
+// ========================= 2 =================== 
+
+// let name = "John";
+// let age = 25;
+// let tech = `Mobile and Application Developer`;
+
+// alert(name);
+// alert(age);
+// alert(tech);
+
+
+// ========================= 3 =================== 
+
+// let email = `iamdanish1998@gmail.com`
+// alert(`my Email Address is ${email}`)
+
+// ========================= 4 =================== 
+
+
+// document.write("Yeah! i can write Javascript Coding ...")
+
+// ========================= 5 ===================
+
+// let age;
+
+//  age = prompt(`eNTER Your Age`)
+
+//  alert(`i am ${age} years old`)
+
+// ========================= 6 =================== 
+
+// let birth_year;
+// birth_year = +prompt(`Enter your Birth Year`);
+// document.write(`My Birth Year is ${birth_year}<br />`);
+// document.write(`data type of my birth year is ` ,typeof birth_year)
+
+// ========================= 7 =================== 
+
+// document.write(`Variable name only contain numbers,$ and _<br />`)
+// document.write(`Variable name must begin with a letter , $ or _ <br />`)
+// document.write(`Variable name are case sensative <br /> `)
+// document.write(`can not be js Keywords`)
+
+// ========================= 8 =================== 
+
+// let a = 5;
+// let b = 10;
+// c = a + b;
+
+// document.write(`The Sum of ${a} and ${b} is ${c}`)
+
+// ========================= 9 =================== 
+
+// var initail_val;
+// document.write(initail_val,"<br />")
+// initail_val = 5;
+// document.write("initial Value is :",initail_val,"<br />")
+// let a = ++initail_val;
+// document.write("Value after increment :",initail_val,"<br />")
+// let sum = initail_val + a; 
+// document.write("Sum of initial Value and increment value :",sum,"<br />")
+// let dec = --sum;
+// document.write("Value after Decrement :",dec,"<br />")
+
+// ========================= 10 =================== 
+
+
+// let c_t = 25;
+// let f_h_t = 70;
+// let ctof = (f_h_t - 32 ) * 5 / 9;
+// let ftoc = (c_t * 9 / 5 ) + 32;
+
+// document.write(`Fahrenheit to Celsius ${ctof} °C <br /> `)
+// document.write(`Celsius to Fahrenheit ${ftoc} °F`)
+
+// ========================= 11 =================== 
+
+// let us = 10;
+// let riy = 25;
+
+// let c_p = 10 * 155 + 25 * 41;
+//  document.write(`Total Currency in PKR : ${c_p}`)
+
+
+// ========================= 12 =================== 
+
+// let a = 10;
+// console.log(`Value of a is ${10}`);
+// ++a;
+// console.log(`the value of ++a is `,a);
+// console.log(`Now the value of a is `,a);
+// a++;
+// console.log(`the value of a++ is `,a);
+// --a;
+// console.log(`the value of --a is `,a);
+// a--;
+// console.log(`the value of a-- is `,a);
+
+// ========================= 13 =================== 
+
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+console.log(result)
+
+// --a; 2
+// --a - --b;2
+// --a - --b + ++b;3
+// --a - --b + ++b + b--;3/4
+
+
+
+
+
+
+
+
+
+
+
+
