93dd69404b440f73250b12cdec0a8b2d8f71cd0b
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-10-07 23:10:03 +0500

    assigment_2

---
+++ assigment_1/assigment2.js
@@ -0,0 +1,169 @@
+// ==================================================
+
+// var city = prompt(`Enter your City Name`);
+// if(city === 'karachi'){
+//     document.write(`Welcome to city of lights`)
+
+// }
+// else{
+//     document.write(`You are not from karachi`)
+// }
+
+// ==================================================
+
+// let colour = prompt("color of road traffic signal");
+// if(colour ==="red"){
+// document.write(`Must Stop`)
+// }
+// if(colour ==="yellow"){
+//     document.write(`Ready to Move`)
+
+// }
+// if(colour ==="green"){
+//     document.write(`Move now`)
+
+// }
+
+// ==================================================
+
+// var a = 4;
+// if (++a === 5) {
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// if (true){
+
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// if("car" < "cat"){
+
+//     alert("car is smaller than cat");
+//     }
+
+// ==================================================
+
+// let sub1 = +prompt(`Enter First Subject Number`);
+// let sub2 = +prompt(`Enter Second Subject Number`);
+// let sub3 = +prompt(`Enter Third Subject Number`);
+// let sum = sub1 + sub2 + sub3 ;
+//  let percentage = (sum / 300) * 100;
+//  console.log(`your Percentage is ${percentage} %`)
+
+//  if(percentage >= 80 ){
+//     console.log(`Grade : A-ONE`);
+//     console.log(`Remarks : Excellent`)
+//  }
+//  else if(percentage >= 70 ){
+//     console.log(`Grade : A`);
+//     console.log(`Remarks : Good`)
+//  }
+//  else if(percentage >= 60 ){
+//     console.log(`Grade : B`);
+//     console.log(`Remarks : Need Improvment`)
+//  }
+//  if(percentage < 60 ){
+//     console.log(`Grade : F`);
+//     console.log(`Remarks : Sorry`)
+//  }
+// ==================================================
+
+// let secret = 4;
+// let num = +prompt(`Enter your choice number`);
+// if(num === secret){
+//     alert(`Bingo You are Right`)
+// }
+// else if(num ==3){
+//     alert(`YOu are Very near`)
+
+// }
+
+// ==================================================
+
+// let a = 51;
+// if(a % 2 == 0){
+//     alert(`even number`)
+// }
+// else{
+//     alert('old')
+// }
+
+// ==================================================
+
+// let num1 = +prompt(`Enter First Number`);
+// let num2 = +prompt(`Enter Second Number`);
+// let op = prompt(`Enter Operation (+ - / * )`);
+
+// if(op == "+"){
+//     alert(num1 + num2)
+// }
+// if(op == "-"){
+//     alert(num1 - num2)
+// }
+// if(op == "/"){
+//     alert(num1 / num2)
+// }
+// if(op == "*"){
+//     alert(num1 * num2)
+// }
+
+// ==================================================
+
+// let num = +prompt(`Enter Number`);
+// if(num == 0){
+//     alert('number is Zero')
+// }
+// else if(num > 0){
+//     alert('number is Positive')
+// }
+// else if(num < 0){
+//     alert('number is Negative')
+// }
+
+// ==================================================
+// let password = prompt("enter your password ");
+
+// if (password == "") {
+//     alert("fileld the field")
+// }
+// else {
+//     let cpassword = prompt("enter your Password Again ");
+
+//     if (password == cpassword) {
+//         alert("Login successfully")
+//     }
+//     else {
+//         alert("Password is NOt Matched")
+//     }
+// }
+
+// ==================================================
